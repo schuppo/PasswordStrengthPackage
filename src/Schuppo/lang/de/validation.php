45f617749768a8567dd9fa5565f89e65d525a737
Oliver Schupp <oliver.schupp@yahoo.de>
2014-02-17 20:02:45 +0100

    added Lang folder with english and german localization files

---
+++ src/Schuppo/lang/de/validation.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+            "letters" => ":attribute muss mindestens einen Buchstaben enthalten.",
+            "case_diff" => ":attribute muss sowohl groß- als auch kleingeschriebene Buchstaben enthalten.",
+            "numbers" => ":attribute muss mindestens eine Zahl enthalten."
+];
---
+++ src/Schuppo/lang/en/validation.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+            "letters" => "The :attribute must include at least one letter.",
+            "case_diff" => "The :attribute must include both upper and lower case letters.",
+            "numbers" => "The :attribute must include at least one number."
+];
