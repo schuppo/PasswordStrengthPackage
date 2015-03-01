859afb3b9f1d0bf83dd5214a99ee3c1d0b6e8b79
schuppo <oliver.schupp@yahoo.de>
2015-03-01 14:39:15 +0100

    adds french translations

---
+++ src/Schuppo/lang/fr/validation.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    "letters" => "Le champ :attribute doit comprendre au moins une lettre.",
+    "case_diff" => "Le champ :attribute doit comprendre des lettres minuscules et majuscules.",
+    "numbers" => "Le champ :attribute doit comprendre au moins un nombre.",
+    "symbols" => "Le champ :attribute doit comprendre au moins un symbole (#,@,$, etc.)."
+];
