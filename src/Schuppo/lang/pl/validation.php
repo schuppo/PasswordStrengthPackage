64776d1fbe17c46ef7c25b906cf4bd52ca164d42
Krystian Duma <git@kduma.tk>
2015-03-06 18:32:22 +0100

    Missing text

--- src/Schuppo/lang/pl/validation.php
+++ src/Schuppo/lang/pl/validation.php
@@ -2,7 +2,7 @@
 
 return [
     "letters"   => "Pole :attribute musi zawierać przynajmniej jedną literę.",
-    "case_diff" => "Pole :attribute musi zawierać jedną małą i jedną dużą literę.",
+    "case_diff" => "Pole :attribute musi zawierać przynajmniej jedną małą i jedną dużą literę.",
     "numbers"   => "Pole :attribute musi zawierać przynajmniej jedną cyfrę.",
     "symbols"   => "Pole :attribute musi zawierać przynajmniej jeden symbol.",
 ];
