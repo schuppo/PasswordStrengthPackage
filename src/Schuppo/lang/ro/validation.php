29380639a1984203e56dd8d769f0ac4533f79de3
Uibar Ion-Cristian <uibar@vision-gate.com>
2015-06-13 15:44:47 +0300

    Romanian Translation

---
+++ src/Schuppo/lang/ro/validation.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+            "letters" => "Câmpul :attribute trebuie să conțină cel puțin o literă.",
+            "case_diff" => "Câmpul :attribute trebuie să conțină atât litere mici cât și litere mari.",
+            "numbers" => "Câmpul :attribute trebuie să conțină cel puțin o cifră.",
+            "symbols" => "Câmpul :attribute trebuie să conțină cel puțin un simbol."
+];
