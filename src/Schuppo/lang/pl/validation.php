8f7f456b4417b7b5a7a5af46725c4a5f58ee6cdf
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2016-10-02 15:19:42 +0200

    Better Polish translation

--- src/Schuppo/lang/pl/validation.php
+++ src/Schuppo/lang/pl/validation.php
@@ -4,5 +4,5 @@ return [
     "letters"   => "Pole :attribute musi zawierać przynajmniej jedną literę.",
     "case_diff" => "Pole :attribute musi zawierać przynajmniej jedną małą i jedną dużą literę.",
     "numbers"   => "Pole :attribute musi zawierać przynajmniej jedną cyfrę.",
-    "symbols"   => "Pole :attribute musi zawierać przynajmniej jeden symbol.",
+    "symbols"   => "Pole :attribute musi zawierać przynajmniej jeden znak specjalny.",
 ];
