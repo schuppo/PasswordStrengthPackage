8f20923e318e97c463b9cec18bcf7ef581aaef7d
Oliver Schupp <post@schuppo.io>
2017-10-12 20:40:42 +0200

    closes #27

---
+++ src/Schuppo/lang/es/validation.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    "letters" => "El campo :attribute debe incluir al menos una letra.",
+    "case_diff" => "El campo :attribute debe incluir letras mayúsculas y minúsculas.",
+    "numbers" => "El campo :attribute debe incluir al menos un número.",
+    "symbols" => "El campo :attribute debe incluir al menos un símbolo.",
+];
