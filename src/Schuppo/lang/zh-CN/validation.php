b586e27ed4bc67fe9f8a33069416af858f6da97d
Oliver Schupp <post@schuppo.io>
2017-10-12 20:39:29 +0200

    closes #28

---
+++ src/Schuppo/lang/zh-CN/validation.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    "letters" => ":attribute 必须包含至少一个字母。",
+    "case_diff" => ":attribute 必须包含大小写字母。",
+    "numbers" => ":attribute 必须包含至少一个数字。",
+    "symbols" => ":attribute 必须包含至少一个符号。",
+];
---
+++ src/Schuppo/lang/zh-TW/validation.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    "letters" => ":attribute 必須至少包含一個字母。",
+    "case_diff" => ":attribute 必須包含大小寫字母。",
+    "numbers" => ":attribute 必須至少包含一個數字。",
+    "symbols" => ":attribute 必須至少包含一個符號。",
+];
