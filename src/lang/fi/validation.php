7107cfd06b0cd1adcc814333c308af7db96f3c6c
Mikko Ala-Fossi <mikko.ala-fossi@ukkopro.fi>
2021-02-01 08:52:28 +0200

    Add Finnish translations

---
+++ src/lang/fi/validation.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    "letters" => ":Attribute täytyy sisältää vähintään yksi kirjain.",
+    "case_diff" => ":Attribute täytyy sisältää iso ja pieni kirjain.",
+    "numbers" => ":Attribute täytyy sisältää vähintään yksi numero.",
+    "symbols" => ":Attribute täytyy sisältää vähintään yksi erikoismerkki.",
+];
