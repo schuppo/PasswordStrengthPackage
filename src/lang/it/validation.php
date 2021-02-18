5566fbaef66d8579fc72b1c225cf7519e1929fe1
Federico Liva <mail@federicoliva.dev>
2021-02-18 11:24:27 +0100

    Add italian translation

---
+++ src/lang/it/validation.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    "letters" => ":attribute deve includere almeno una lettera.",
+    "case_diff" => ":attribute deve includere sia lettere maiuscole che minuscole.",
+    "numbers" => ":attribute deve includere almeno un numero.",
+    "symbols" => ":attribute deve includere almeno un simbolo.",
+];
